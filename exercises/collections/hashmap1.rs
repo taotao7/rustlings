f0b97d18b119a11edcd1c9a7493b2a9ddd4a8270
xt01102058 <xt01102058@alibaba-inc.com>
2022-07-07 15:08:15 +0800

    feat: add hashmap

--- exercises/collections/hashmap1.rs
+++ exercises/collections/hashmap1.rs
@@ -11,15 +11,18 @@
 // Execute the command `rustlings hint hashmap1` if you need
 // hints.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 fn fruit_basket() -> HashMap<String, u32> {
-    let mut basket = // TODO: declare your hash map here.
+    let mut basket = HashMap::new(); // TODO: declare your hash map here.
 
     // Two bananas are already given for you :)
     basket.insert(String::from("banana"), 2);
+    basket.insert(String::from("xxxx"), 3);
+    basket.insert(String::from("12"), 0);
+    basket.insert(String::from("xxx"), 1);
+    basket.insert(String::from("xxdasdxx"), 4);
+    basket.insert(String::from("xxxssssx"), 5);
 
     // TODO: Put more fruits in your basket here.
 
